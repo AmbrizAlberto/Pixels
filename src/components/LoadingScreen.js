f64dc42b6f3fd7bba84ddc03ccfbeb31d3caf404
AmbrizAlberto <albertpoambez@gmail.com>
2024-06-10 23:53:25 -0600

    LoadingScreen Updated for fix the issues

--- src/components/LoadingScreen.js
+++ src/components/LoadingScreen.js
@@ -1,10 +1,24 @@
-import React from 'react';
-import Spinner from './spinner';
+'use client';
+
+import React, { useEffect, useState } from 'react';
+import "../../public/css/spinner.css"; // Ensure the correct path to the CSS file
 
 const LoadingScreen = () => {
+  const [hydrated, setHydrated] = useState(false);
+
+  useEffect(() => {
+    setHydrated(true);
+  }, []);
+
+  if (!hydrated) {
+    return null;
+  }
+
   return (
     <div className="loading-screen">
-      <Spinner/>
+      <div className="spinner">
+        <span className="loaderspinner"></span>
+      </div>
     </div>
   );
 };
