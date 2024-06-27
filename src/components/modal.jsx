aa4431b33ad63138a4d05505400d5f24241d93f1
AmbrizAlberto <albertpoambez@gmail.com>
2024-06-26 21:18:04 -0600

    New Post v1

--- src/components/modal.jsx
+++ src/components/modal.jsx
@@ -1,20 +1,43 @@
 // components/Modal.jsx
-
+'use client'
 import React from 'react';
 import "../../public/css/modal.css";
+import { useRouter } from 'next/navigation';
 
 const Modal = ({ isOpen, onClose }) => {
   if (!isOpen) return null;
+
+  const router = useRouter();
+
+  const onSubmit = async(e) => {
+    e.preventDefault()
+    const title = e.target.title.value
+    const description = e.target.description.value
+
+    const res = await fetch ('/api/posts', {
+      method: 'POST',
+      body: JSON.stringify({title, description}),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+
+    const data = await res.json()
+    console.log(data)
+
+    onClose(true)
+
+  }
 
   return (
     <div className="modal-overlay" onClick={onClose}>
       <div className="modal-content" onClick={(e) => e.stopPropagation()}>
         <span className="close" onClick={onClose}>&times;</span>
         <h2>Crear Publicación</h2>
-        <form>
-          <label htmlFor="photo">Fotografía</label>
+        <form onSubmit={onSubmit}>
+          {/* <label htmlFor="photo">Fotografía</label>
           <input type="file" id="photo" name="photo" accept="image/*" required />
-
+ */}
           <label htmlFor="title">Título</label>
           <input type="text" id="title" name="title" required />
 
