9254d64935b10be7fcac9bba44c30061da981c1c
AmbrizAlberto <albertpoambez@gmail.com>
2024-06-28 13:00:16 -0600

    Posts are shown in Home

--- src/components/posts.jsx
+++ src/components/posts.jsx
@@ -1,122 +1,66 @@
 // posts.jsx
-
-/* IMPORT PRINCIPAL LIBRARYS */
+import { useEffect, useState } from 'react';
 import Link from 'next/link';
-import prisma from '@/libs/db';
-
-/* IMPORT CSS */
-import "../../public/css/posts.css"
-
-/* BOOTSTRAP ICONS */
 import 'bootstrap-icons/font/bootstrap-icons.css';
+import "../../public/css/posts.css";
 
+export default function Posts() {
+  const [posts, setPosts] = useState([]);
 
-/* async function loadPosts(){
-  return await prisma.post.findMany()
-}
- */
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const response = await fetch('/api/posts');
+        const data = await response.json();
+        setPosts(data);
+      } catch (error) {
+        console.error("Error fetching posts:", error);
+      }
+    };
 
-export default /* async */ function Posts() {
-
-  /* const posts = await loadPosts();
-  console.log(posts);
- */
+    fetchPosts();
+  }, []);
 
   return (
-
     <div className='Posts'>
       <br />
-
-      <div className="publis">
-        <div className='containerpost'>
-
-          {/* INFO DE USUARIO */}
-          <div className='infouser'>
-            <div className='imguser'>
-              <img src="https://3.bp.blogspot.com/-MAtrroD5mj8/UKl-JropN-I/AAAAAAABQ9Y/2kIMSnm_4Jw/s1600/Beletsi_Lake_Parnitha_HDR_.jpg" alt="" />
+      {posts.map(post => (
+        <div key={post.id} className="publis">
+          <div className='containerpost'>
+            <div className='infouser'>
+              <div className='imguser'>
+                <img src="https://3.bp.blogspot.com/-MAtrroD5mj8/UKl-JropN-I/AAAAAAABQ9Y/2kIMSnm_4Jw/s1600/Beletsi_Lake_Parnitha_HDR_.jpg" alt="" />
+              </div>
+              <div className='nameuser'>
+                AlDev
+              </div>
             </div>
-            <div className='nameuser'>
-              AlDev
+            <div className='contain'>
+              <div className='photopost'>
+                <img src="https://i.pinimg.com/originals/a9/95/63/a995634384bf179509c82723aa748af1.jpg" alt="" />
+              </div>
+              <div className='photodesc'>
+                <h1>{post.title}</h1>
+                <h1>{post.description}</h1>
+                <h1>Propiedades</h1>
+                <h2><i className="bi bi-camera"></i> Sony</h2>
+                <h2><i className="bi bi-fan"></i> f1.2</h2>
+              </div>
             </div>
-          </div>
-
-          {/* DIV DE FOTO Y DESCRIPCION */}
-          <div className='contain'>
-            {/* DERECHA */}
-            <div className='photopost'>
-              <img src="https://i.pinimg.com/originals/a9/95/63/a995634384bf179509c82723aa748af1.jpg" alt="" />
-            </div>
-            {/* IZQUIERDA */}
-            <div className='photodesc'>
-              <h1>Titulo</h1>
-              <h1>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ullam, aperiam consequatur quis dolorum, vitae adipisci vel excepturi quas rerum sint molestias molestiae. Beatae minus a id. Iure harum molestias doloribus?</h1>
-              <h1>Propiedades</h1>
-              <h2><i className="bi bi-camera"></i> Sony</h2>
-              <h2><i className="bi bi-fan"></i> f1.2</h2>
-            </div>
-          </div>
-
-          {/* ABAJO */}
-          <div className='postabajo'>
-            <div class="resumen">
-              <p>10,000 Likes</p>
-              <p>10,000 Comentarios</p>
-            </div>
-            <div class="reactions">
-              <button className="btn btn-primary"><i class="bi bi-star"></i></button>
-              <button className="btn btn-secondary"><i class="bi bi-chat"></i></button>
+            <div className='postabajo'>
+              <div className="resumen">
+                <p>10,000 Likes</p>
+                <p>10,000 Comentarios</p>
+              </div>
+              <div className="reactions">
+                <button className="btn btn-primary"><i className="bi bi-star"></i></button>
+                <button className="btn btn-secondary"><i className="bi bi-chat"></i></button>
+              </div>
             </div>
           </div>
-
         </div>
-      </div>
-
-      <div className="publis">
-        <div className='containerpost'>
-
-          {/* INFO DE USUARIO */}
-          <div className='infouser'>
-            <div className='imguser'>
-              <img src="https://3.bp.blogspot.com/-MAtrroD5mj8/UKl-JropN-I/AAAAAAABQ9Y/2kIMSnm_4Jw/s1600/Beletsi_Lake_Parnitha_HDR_.jpg" alt="" />
-            </div>
-            <div className='nameuser'>
-              AlDev
-            </div>
-          </div>
-
-          {/* DIV DE FOTO Y DESCRIPCION */}
-          <div className='contain'>
-            {/* DERECHA */}
-            <div className='photopost'>
-              <img src="https://3.bp.blogspot.com/-MAtrroD5mj8/UKl-JropN-I/AAAAAAABQ9Y/2kIMSnm_4Jw/s1600/Beletsi_Lake_Parnitha_HDR_.jpg" alt="" />
-            </div>
-            {/* IZQUIERDA */}
-            <div className='photodesc'>
-              <h1>Titulo</h1>
-              <h1>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ullam, aperiam consequatur quis dolorum, vitae adipisci vel excepturi quas rerum sint molestias molestiae. Beatae minus a id. Iure harum molestias doloribus?</h1>
-              <h1>Propiedades</h1>
-              <h2><i className="bi bi-camera"></i> Sony</h2>
-              <h2><i className="bi bi-fan"></i> f1.2</h2>
-            </div>
-          </div>
-
-          {/* ABAJO */}
-          <div className='postabajo'>
-            <div class="resumen">
-              <p>10,000 Likes</p>
-              <p>10,000 Comentarios</p>
-            </div>
-            <div class="reactions">
-              <button className="btn btn-primary"><i class="bi bi-star"></i></button>
-              <button className="btn btn-secondary"><i class="bi bi-chat"></i></button>
-            </div>
-          </div>
-
-        </div>
-      </div>
-
+      ))}
       <br />
     </div>
-  
   );
 }
